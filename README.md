3bb2e00a754d64c00ace2a46facd38ff2773fecf
Amit <amit-c@everledger.io>
2022-08-05 15:13:24 +0530

    updated code

--- README.md
+++
@@ -1,6 +0,0 @@
-# Create React App example
-
-## How to use
-
-Download the example [or clone the repo](https://github.com/mui/material-ui):
-
--- app.js
+++ app.js
@@ -1,12 +1,9 @@
+const express = require('express')
 const path = require('path');
-const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '', 'build');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+
+const app = express()
+const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
